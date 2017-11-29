2fb377bfe558b0aff8d8757f55907c7fa9447ba3
Andrew Piper <piper.andrew@spcollege.edu>
2017-11-29 13:11:21 -0500

    Changed how models are loaded by getting them directly from the target assembly.

---
+++ ModelSchemas/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Threax.AspNetCore.Models;
+
+namespace DevApp.ModelSchemas
+{
+    //[PluralName("ValueWoots")]
+    public class Value
+    {
+        [Required]
+        [MaxLength(450)]
+        //[Display(Name = "WootName")]
+        public String Name { get; set; }
+    }
+}
