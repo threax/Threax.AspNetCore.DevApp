1b5096ce4f5156cfb3d817edc2e823828baaedef
Andrew Piper <piper.andrew@spcollege.edu>
2017-12-04 08:21:12 -0500

    Moved some more extension data to the json extensions class. Added the concept of a virtual property that will allow overrides, still not implemented in generators.

---
+++ ModelSchemas/HorribleBeast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Threax.AspNetCore.Models;
+
+namespace DevApp.ModelSchemas
+{
+    [PluralName("HorribleBeasties")]
+    public abstract class HorribleBeast
+    {
+        [Required]
+        [MaxLength(450)]
+        public String Name { get; set; }
+
+        public int NumEyes { get; set; }
+
+        public virtual int NumLegs { get; set; }
+    }
+}
